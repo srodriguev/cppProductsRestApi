5a8051ab5b8ffb46c2e8d92077a0c8c85af5edf4
srodriguev <srodriguev@eafit.edu.co>
2024-03-21 09:19:57 -0500

    unit fix

--- src/unitTest.cpp
+++ src/unitTest.cpp
@@ -14,8 +14,8 @@ json::value createJsonResponse(int id, const std::string name, double price) {
 }
 
 
-TEST(GetDataTest, ValidId) {
-    int id = 1;
+TEST(GetDataTest, ValidProduct) {
+    int id = 2;
     //Data de prueba controlada:
     std::map<int, std::pair<std::string, double>> testData = {
         {1, {"Producto 1", 10.5}}, 
@@ -25,8 +25,8 @@ TEST(GetDataTest, ValidId) {
     json::value result = getData(id,testData);
 
     ASSERT_EQ(result[U("id")].as_integer(), id);
-    ASSERT_EQ(result[U("name")].as_string(), "Producto 1");
-    ASSERT_EQ(result[U("price")].as_double(), 10.5);
+    ASSERT_EQ(result[U("name")].as_string(), "Producto 2");
+    ASSERT_EQ(result[U("price")].as_double(), 5.0);
 }
 
 TEST(GetDataTest, InvalidId) {
@@ -52,3 +52,35 @@ TEST(GetDataTest, EmptyFile) {
     ASSERT_TRUE(result.is_null()); 
 }
 
+TEST(AddRecordTests, AddValidRecord) {
+    
+    int id = 99999;
+    std::string name = "Test Product";
+    double price = 9.9999;
+
+    // Act: Attempt to add a record.
+    bool resultAdded = insertRecord(id, name, price);
+
+    // Assert: Verify the record was added successfully.
+    ASSERT_TRUE(resultAdded);
+
+    //Check if it exists
+    std::map<int, std::pair<std::string, double>> data = loadDataFromCSV("data.csv");
+    
+    json::value result = getData(id,data);
+
+    ASSERT_EQ(result[U("id")].as_integer(), id);
+    ASSERT_EQ(result[U("name")].as_string(), name);
+    ASSERT_EQ(result[U("price")].as_double(), price);
+
+    bool resultDelete = deleteRecord(id);
+    ASSERT_TRUE(resultDelete);
+}
+
+
+TEST(DeleteRecordTests, deleteNotFound) {
+    int id = 999990; //Should not exists
+
+    bool resultDelete = deleteRecord(id);
+    ASSERT_FALSE(resultDelete);
+}
